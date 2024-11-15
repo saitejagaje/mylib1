de1896cc01fe3d8fe5cd0d52c3e7f6dcb7c10e2c
saitejagaje <gajesaiteja22@gmail.com>
2024-11-15 16:47:37 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -12,5 +12,5 @@ def deployment(jobname,ipaddress,appname)
 }
 def selenium(jobname)
 {
-   sh 'java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar
+   sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
