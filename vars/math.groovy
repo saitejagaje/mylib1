410dd6d3a00cd3d5d5d552226165402da4570665
saitejagaje <gajesaiteja22@gmail.com>
2024-11-15 15:27:58 +0530

    Update math.groovy
    maths

--- vars/math.groovy
+++ vars/math.groovy
@@ -1,1 +1,16 @@
-
+def sum(a,b)
+{
+  echo "the sum of ${a} and ${b} is ${a+b}"
+}
+def sub(a,b)
+{
+  echo "the substraction of ${a} and ${b} is ${a-b}" 
+}
+def mul(a,b)
+{
+  echo "the multiplication of ${a} and ${b} is ${a*b}"
+}
+def div(a,b)
+{
+  echo "the division of ${a} and ${b} is ${a/b}"
+}
