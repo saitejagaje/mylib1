9b911a12d9b973acd61af7ccc0ddcfb992aa22bb
saitejagaje <gajesaiteja22@gmail.com>
2024-11-19 11:37:40 +0530

    Update cicd1.groovy

--- vars/cicd1.groovy
+++ vars/cicd1.groovy
@@ -7,6 +7,10 @@ def buildArtifact()
 {
    sh 'mvn package'
 }
+def deploy(ip,path)
+{
+   deploy adapters: [tomcat9(credentialsId: 'new-credentials', path: '', url: 'http://${ip}:8080')], contextPath: '${path}', war: '**/*.war'
+}
 def runSelenium(jobname)
 {
    sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
